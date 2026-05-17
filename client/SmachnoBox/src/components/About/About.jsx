5d5b488b8e568dad134ed9fb3f27d83c74c2e74c
mariancompany7-alt <mariancompany7@gmail.com>
2026-05-17 20:55:56 +0300

    Update project 04

--- client/SmachnoBox/src/components/About/About.jsx
+++ client/SmachnoBox/src/components/About/About.jsx
@@ -1,8 +1,11 @@
 import React from 'react';
 import styles from './About.module.css';
+import Hero from '../Hero.jsx/Hero';
 
 function About() {
-  
+    return (
+        <Hero />
+    )
 }
 
 export default About;
---
+++ client/SmachnoBox/src/components/Hero.jsx/Hero.jsx
@@ -0,0 +1,19 @@
+import React from 'react'
+import styles from './Hero.module.css'
+
+function Hero() {
+    return (
+        <section className={styles.hero}>
+            <div className={styles.overlay}>
+                <div className={styles.content}>
+                    <h1>Смачно. Корисно. Швидко.</h1>
+                    <p>Твій персональний раціон здорового харчування з доставкою до дверей.</p>
+                    {/* Кнопка, яка плавно скролить до меню */}
+                    <a href="#menu" className={styles.ctaButton}>Обрати раціон</a>
+                </div>
+            </div>
+        </section>
+    )
+}
+
+export default Hero
